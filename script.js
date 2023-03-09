7dcca4d9c4a7ab540963291431a4c92d06b27de2
marco <macmwz20@gmail.com>
2023-03-09 14:59:35 +1300

    0

--- script.js
+++ script.js
@@ -19,13 +19,39 @@ function clearOutput() {
 
 function calculate() {
     const expression = calculationElement.textContent;
-    const result = eval(expression);
+
+    // Use regular expressions to split the expression into numbers and operators
+    const numbers = expression.split(/[\+\-\*\/]/).map(n => parseFloat(n));
+    const operators = expression.split(/[0-9\.]+/).filter(op => op !== '');
+
+    // Evaluate multiplication and division before addition and subtraction
+    let result = numbers[0];
+    for (let i = 0; i < operators.length; i++) {
+        const operator = operators[i];
+        const nextNumber = numbers[i + 1];
+        if (operator === '*') {
+            result *= nextNumber;
+        } else if (operator === '/') {
+            result /= nextNumber;
+        } else if (operator === '+') {
+            result += nextNumber;
+        } else if (operator === '-') {
+            result -= nextNumber;
+        }
+    }
 
     if (result === Infinity || result === -Infinity) {
         answerElement.textContent = 'Error';
     } else {
-        answerElement.textContent = `= ${result}`;
+        answerElement.textContent = `= ${result.toLocaleString()}`;
     }
-    output = '0';
+    output = result.toString();
 }
 
+function backspace() {
+    output = output.slice(0, -1);
+    if (output === '') {
+        output = '0';
+    }
+    calculationElement.textContent = output;
+}
